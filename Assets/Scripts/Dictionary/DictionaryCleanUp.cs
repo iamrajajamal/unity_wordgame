e8c96246c63b0e31fecba4028c2395f682bb87a7
iamrajajamal <martian.hacked@gmail.com>
2019-03-27 20:21:35 +0500

    Working on Dictionary

---
+++ Assets/Scripts/Dictionary/DictionaryCleanUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DictionaryCleanUp : MonoBehaviour
+{
+    public string inputFile;
+
+    public string outputFile;
+
+    public int minLength = 3;
+
+    public char[] badChars = new char[]
+    {
+        ' ', '.', '-', '\'', '1',
+        '2', '3', '4', '5', '6',
+        '7', '8', '9', '0', ':'
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (string.IsNullOrEmpty(inputFile))
+            throw new Exception("Missing Input File!");
+
+        if (string.IsNullOrEmpty(outputFile))
+            throw new Exception("Missing Output File!");
+
+        StartCoroutine("LoadWordList");
+    }
+    /*
+    IEnumerator LoadWordList()
+    {
+
+    }
+    */
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Dictionary/WordListLoader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class WordListLoader : MonoBehaviour
+{
+    public string fileName = "words/1k.txt";
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("LoadWordList");
+    }
+
+    IEnumerator LoadWordList()
+    {
+        string filePath = Path.Combine(
+            Application.streamingAssetsPath, fileName);
+
+        string result = null;
+        if (filePath.Contains("://"))
+        {
+            WWW www = new WWW(filePath);
+            yield return www;
+            result = www.text;
+        }
+        else
+        {
+            result = File.ReadAllText(filePath);
+            var words = result.Split('\n');
+
+            foreach (var w in words)
+            {
+                var word = w.TrimEnd();
+                if (string.IsNullOrEmpty(word))
+                {
+                    continue;
+                }
+                Debug.Log(word);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
